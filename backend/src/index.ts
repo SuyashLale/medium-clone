3b8ed005d23a057d3c7755d95e70fc9b8c953186
SuyashLale <suyash.lale1990@gmail.com>
2024-03-10 13:02:09 +0530

    Updated .gitignore

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,14 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
-
 app.post("/api/v1/signup", (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
   return c.text("Sign up route");
 });
 
