3ab4175ca4bf539922f6cd8879d47e849683f24a
SuyashLale <suyash.lale1990@gmail.com>
2024-03-10 14:26:41 +0530

    Added sign up and sign in routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,98 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign, verify, decode } from "hono/jwt";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
 
-app.post("/api/v1/signup", (c) => {
+app.post("/api/v1/signup", async (c) => {
   const prisma = new PrismaClient({
-    datasourceUrl: env.DATABASE_URL,
+    datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
 
-  return c.text("Sign up route");
+  // Get the body from the context.
+  const body = await c.req.json();
+
+  try {
+    const dbLookup = await prisma.user.findFirst({
+      where: {
+        email: body.email,
+      },
+    });
+
+    if (dbLookup) {
+      c.status(411);
+      return c.json({
+        error: "Email already taken",
+      });
+    }
+
+    // Create the user record
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+
+    // Create the JWT token to be sent back to the user
+    const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+
+    c.status(200);
+    return c.json({
+      jwt: token,
+    });
+  } catch (e) {
+    console.log("Error while signing up", e);
+    c.status(403);
+    return c.json({
+      error: "Error while signing up",
+    });
+  }
 });
 
-app.post("/api/v1/signin", (c) => {
-  return c.text("Sign in route");
+app.post("/api/v1/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  // Get the request body
+  const body = await c.req.json();
+
+  try {
+    // Get the email, pwd from the user in the body and check in DB
+    const user = await prisma.user.findUnique({
+      where: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+
+    if (!user) {
+      c.status(403);
+      return c.json({
+        error: "Incorrect credentials. No user found",
+      });
+    }
+
+    // Generate the JWT token
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    c.status(200);
+    return c.json({
+      jwt,
+    });
+  } catch (e) {
+    console.log("Error in sign in route", e);
+    c.status(403);
+    return c.json({
+      error: "Error in sign in route",
+    });
+  }
 });
 
 app.post("api/v1/blog)", (c) => {
